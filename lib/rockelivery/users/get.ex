4a33746a4f38e9536aadea0b35b64705964cc151
Rômulo Silva <romulo_lss@hotmail.com>
2021-11-29 20:08:22 -0300

    refac: improving to error messages

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,27 +1,17 @@
 defmodule Rockelivery.Users.Get do
   alias Ecto.UUID
-  alias Rockelivery.{Repo, User}
-
-  def by_id2(id) do
-    with {:ok, uuid} <- UUID.cast(id),
-         %User{} = user <- Repo.get(User, uuid) do
-      {:ok, user}
-    else
-      :error -> {:error, %{status: :bad_request, result: "Invalid id format!"}}
-      nil -> {:error, %{status: :not_found, result: "User not found!"}}
-    end
-  end
+  alias Rockelivery.{Error, Repo, User}
 
   def by_id(id) do
     case UUID.cast(id) do
-      :error -> {:error, %{status: :bad_request, result: "Invalid id format!"}}
+      :error -> {:error, Error.build_id_format_error()}
       {:ok, uuid} -> get(uuid)
     end
   end
 
   defp get(id) do
     case Repo.get(User, id) do
-      nil -> {:error, %{status: :not_found, result: "User not found!"}}
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
     end
   end

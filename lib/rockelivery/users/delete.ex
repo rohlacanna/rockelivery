c10a74283f5279cae003085f47c2726df3058497
Rômulo Silva <romulo_lss@hotmail.com>
2021-11-29 21:13:31 -0300

    refac: refactoring modules using Plug

--- lib/rockelivery/users/delete.ex
+++ lib/rockelivery/users/delete.ex
@@ -1,15 +1,7 @@
 defmodule Rockelivery.Users.Delete do
-  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
   def call(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, uuid} -> delete(uuid)
-    end
-  end
-
-  defp delete(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> Repo.delete(user)
--- lib/rockelivery/users/update.ex
+++ lib/rockelivery/users/update.ex
@@ -1,15 +1,7 @@
 defmodule Rockelivery.Users.Update do
-  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
   def call(%{"id" => id} = params) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, _uuid} -> update(params)
-    end
-  end
-
-  defp update(%{"id" => id} = params) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> do_update(user, params)

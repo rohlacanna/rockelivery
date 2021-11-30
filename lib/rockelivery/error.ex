bf3b79a1e1462f3748f490a80dfcba517c169fff
Rômulo Silva <romulo_lss@hotmail.com>
2021-11-29 21:01:03 -0300

    feat: creating my first plug

--- lib/rockelivery/error.ex
+++ lib/rockelivery/error.ex
@@ -13,5 +13,4 @@ defmodule Rockelivery.Error do
   end
 
   def build_user_not_found_error, do: build(:not_found, "User not found")
-  def build_id_format_error, do: build(:bad_request, "Invalid id format")
 end

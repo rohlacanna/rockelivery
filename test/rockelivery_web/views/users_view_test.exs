bcb2ae0c47a7f5055fb7b2ef8b431ba87842f169
Rômulo Silva <romulo_lss@hotmail.com>
2021-12-02 22:14:07 -0300

    tests: testing our view

---
+++ test/rockelivery_web/views/users_view_test.exs
@@ -0,0 +1,31 @@
+defmodule RockeliveryWeb.UsersViewTest do
+  use RockeliveryWeb.ConnCase, async: true
+
+  import Phoenix.View
+  import Rockelivery.Factory
+
+  alias RockeliveryWeb.UsersView
+
+  test "renders create.json" do
+    user = build(:user)
+
+    response = render(UsersView, "create.json", user: user)
+
+    assert %{
+             message: "User created!",
+             user: %Rockelivery.User{
+               address: "Rua das bananeiras, 15",
+               age: 23,
+               cep: "12345678",
+               cpf: "12345678901",
+               email: "romulo@banana.com",
+               id: "a67fc24b-9145-4d67-a658-917ed1c50d2a",
+               inserted_at: nil,
+               name: "Rômulo",
+               password: "123456",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
+  end
+end

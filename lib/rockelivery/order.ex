567acd3bdc9f57eeda01df91c3a9fb7760c5c190
Rômulo Silva <romulo_lss@hotmail.com>
2022-02-21 19:45:41 -0300

    feat(requests): creating the order schema and relationships

---
+++ lib/rockelivery/order.ex
@@ -0,0 +1,34 @@
+defmodule Rockelivery.Order do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Enum
+  alias Rockelivery.{Item, User}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:address, :comments, :payment_method, :user_id]
+  @payment_methods [:money, :credit_card, :debit_card]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "items" do
+    field :address, :string
+    field :comments, :string
+    field :payment_method, Enum, values: @payment_methods
+
+    many_to_many :items, Item, join_through: "orders_items"
+    belongs_to :user, User
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params, items) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> put_assoc(:items, items)
+    |> validate_length(:address, min: 10)
+    |> validate_length(:comments, min: 6)
+  end
+end
--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -3,6 +3,7 @@ defmodule Rockelivery.User do
   import Ecto.Changeset
 
   alias Ecto.Changeset
+  alias Rockelivery.Order
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
@@ -19,6 +20,8 @@ defmodule Rockelivery.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :name, :string
+
+    has_many :orders, Order
 
     timestamps()
   end

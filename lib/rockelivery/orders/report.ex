d1e1606a8f3b1c05405336a5257af71233f45903
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-11 19:14:14 -0300

    feat: creating the order report

--- lib/rockelivery/orders/report.ex
+++ lib/rockelivery/orders/report.ex
@@ -1,7 +1,7 @@
 defmodule Rockelivery.Orders.Report do
   import Ecto.Query
 
-  alias Rockelivery.{Order, Repo}
+  alias Rockelivery.{Item, Order, Repo}
 
   @default_block_size 500
 
@@ -13,8 +13,17 @@ defmodule Rockelivery.Orders.Report do
       |> Repo.stream(max_rows: @default_block_size)
       |> Stream.chunk_every(@default_block_size)
       |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
-      |> Enum.into([])
-      |> IO.inspect()
+      |> Enum.map(&parse_line/1)
     end)
+  end
+
+  defp parse_line(%Order{user_id: user_id, payment_method: payment_method, items: items}) do
+    items_string = Enum.map(items, &item_string/1)
+
+    "#{user_id},#{payment_method},#{items_string}#{50.0}\n"
+  end
+
+  defp item_string(%Item{category: category, description: description, price: price}) do
+    "#{category},#{description},#{price},"
   end
 end

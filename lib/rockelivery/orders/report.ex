3c5737e0836dd47c97e214efdbb2788dfc7e86fb
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-11 19:05:44 -0300

    feat: loading items in blocks

--- lib/rockelivery/orders/report.ex
+++ lib/rockelivery/orders/report.ex
@@ -3,12 +3,16 @@ defmodule Rockelivery.Orders.Report do
 
   alias Rockelivery.{Order, Repo}
 
+  @default_block_size 500
+
   def create(filename \\ "report.csv") do
     query = from order in Order, order_by: order.user_id
 
     Repo.transaction(fn ->
       query
-      |> Repo.stream()
+      |> Repo.stream(max_rows: @default_block_size)
+      |> Stream.chunk_every(@default_block_size)
+      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
       |> Enum.into([])
       |> IO.inspect()
     end)

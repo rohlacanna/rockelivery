047c34fccf37dbfdb92e03f08d1bd9d392833ec9
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-11 15:07:06 -0300

    feat: validating username and password

--- lib/rockelivery_web/auth/guardian.ex
+++ lib/rockelivery_web/auth/guardian.ex
@@ -1,7 +1,7 @@
 defmodule RockeliveryWeb.Auth.Guardian do
   use Guardian, otp_app: :rockelivery
 
-  alias Rockelivery.User
+  alias Rockelivery.{Error, User}
   alias Rockelivery.Users.Get, as: UserGet
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
@@ -11,4 +11,17 @@ defmodule RockeliveryWeb.Auth.Guardian do
     |> Map.get("sub")
     |> UserGet.by_id()
   end
+
+  def authenticate(%{"id" => user_id, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- UserGet.by_id(user_id),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token}
+    else
+      false -> {:error, Error.build(:unauthorized, "Please verify your credentials")}
+      error -> error
+    end
+  end
+
+  def authenticate(_), do: {:error, Error.build(:bad_request, "Invalid or missing params")}
 end

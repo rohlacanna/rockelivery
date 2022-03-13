67b696730bd7d591649eb57825a7080fcab72102
Rômulo Silva <romulo_lss@hotmail.com>
2022-03-12 23:06:54 -0300

    feat: scheduling report generation

---
+++ lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,36 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  use GenServer
+
+  require Logger
+
+  alias Rockelivery.Orders.Report
+
+  # CLIENT
+
+  def start_link(_initial_stack) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # SERVER
+
+  @impl true
+  def init(state) do
+    schedule_report_generation()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:generate, state) do
+    Logger.info("Generating report...")
+
+    Report.create()
+    schedule_report_generation()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 60)
+  end
+end

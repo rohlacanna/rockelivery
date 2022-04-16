e667bba4c7d9d9177055d39f3bd1db5890f9e405
Rômulo Silva <romulo_lss@hotmail.com>
2022-04-16 01:27:20 -0300

    docs: updating lib ecto_sql version

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Rockelivery.MixProject do
     [
       {:phoenix, "~> 1.6.2"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.7"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:telemetry_metrics, "~> 0.6"},
